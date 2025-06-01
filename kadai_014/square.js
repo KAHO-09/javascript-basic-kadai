f563f36efbe6a3fed711befc2a2c169ff4036015
KAHO-09 <kahosaegusa@gmail.com>
2025-06-01 15:32:04 +0900

    Update square.js

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,6 +1,4 @@
-const calculateTotal = (price) => {
-    // 関数の引数に「10」を渡して実行した場合、10の二乗の計算結果である「100」を戻り値として返す。
-    console.log(price * price);
+ const calculateTotal = (price) => {
+   return price * price;
 }
-// コンソールに「100」と表示します。
-calculateTotal(10);
+console.log(calculateTotal(10))
